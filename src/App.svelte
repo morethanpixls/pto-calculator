16654cfbc040f12e9410d4a55c170e7fd48b3541
Orlando Arias <hello@orlandomario.com>
2020-07-18 22:12:38 -0400

    Initial Commit and Build

---
+++ src/App.svelte
@@ -0,0 +1,19 @@
+<script>
+</script>
+
+<main>
+    <h1>PTO</h1>
+    <div class="card">
+        <input type="date">
+        <input type="submit" value="Save Date">
+    </div>
+</main>
+
+<style>
+.card {
+    background-color: #fff;
+    border-radius: 20px;
+    padding: 20px;
+}
+
+</style>
