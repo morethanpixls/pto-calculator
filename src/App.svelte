2a52f930cbd97f62962c7ac4ff2905bb270efe04
Orlando Arias <hello@orlandomario.com>
2020-07-18 22:38:33 -0400

    Added message that specifies how many days left of PTO

--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,37 @@
 <script>
+    export let initialPTO;
+
+    initialPTO = 30;
 </script>
 
 <main>
     <h1>PTO</h1>
+
+    <div class="message">
+        {#if initialPTO === 0 }
+            You have no days left.
+        {:else if initialPTO === 1 }
+            You have {initialPTO} day left.
+        {:else}
+            You have {initialPTO} days left.
+        {/if}
+    </div>
+    
     <div class="card">
         <input type="date">
-        <input type="submit" value="Save Date">
+        <input type="submit" value="Add Date">
     </div>
 </main>
 
 <style>
+
+.message {
+    font-family: 'Abril Fatface';
+    font-size: 3rem;
+    padding: 20px;
+    text-align: center;
+}
+
 .card {
     background-color: #fff;
     border-radius: 20px;
